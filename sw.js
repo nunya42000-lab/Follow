c6387176fde2e1b50f12c7c1e2d89978a35383a3
nunya42000-lab <nunya42000@gmail.com>
2025-11-18 08:07:32 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // --- CACHE VERSION ---
 // Change this every time you update any file!
-const CACHE_NAME = 'follow-me-v16'; // <-- Changed to v16
+const CACHE_NAME = 'follow-me-v17'; // <-- Changed to v16
 // --- ---------------- ---
 
 const FILES_TO_CACHE = [
